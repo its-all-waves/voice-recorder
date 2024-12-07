cd5e82b1a432ced9a98b4ab5e08242fa1eae4a7c
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2024-12-07 16:52:31 -0600

    0.6.8

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react({
+    jsxRuntime: 'classic'  // Add this line inside react plugin config
+  })],
   server: {
     open: 'dev/index.html',
   },
