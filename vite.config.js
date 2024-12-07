aee95d7cfcce0b949c8226c47dce78067bc81296
Ian Painter <95984061+its-all-waves@users.noreply.github.com>
2024-12-07 17:49:59 -0600

    add: peak meter as src code, not npm package bundle

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,12 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import commonjs from '@rollup/plugin-commonjs';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    commonjs({
-      requireReturnsDefault: true,
-      include: /node_modules/
-    })
-  ],
+  plugins: [react()],
+  server: {
+    open: 'dev/index.html',
+  },
   build: {
     lib: {
       entry: './src/VoiceRecorder.jsx',
@@ -18,15 +15,13 @@ export default defineConfig({
       formats: ['es', 'umd']
     },
     rollupOptions: {
-      external: ['react', 'react-dom', 'react/jsx-runtime', 'web-audio-peakmeter-react'],
+      external: ['react', 'react-dom'],
       output: {
         globals: {
           react: 'React',
-          'react-dom': 'ReactDOM',
-          'react/jsx-runtime': 'React',
-          'web-audio-peakmeter-react': 'Peakmeter'
+          'react-dom': 'ReactDom',
         }
       }
     }
-  }
-});
+  },
+})
